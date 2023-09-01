d6dd522e712efa4c0a450534b67ed6b965cd5830
alepocalypse <48845086+alepocalypse@users.noreply.github.com>
2023-08-31 17:25:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,59 @@ function saveChecklist() {
 
     localStorage.setItem('checklistItems', JSON.stringify(items));
 }
+
+const actsContainer = document.getElementById('acts');
+
+// Function to render the checklist
+function renderChecklist() {
+    actsContainer.innerHTML = '';
+
+    checklistData.acts.forEach((act) => {
+        const actItem = document.createElement('li');
+        actItem.textContent = act.name;
+
+        const areasList = document.createElement('ul');
+        act.areas.forEach((area) => {
+            const areaItem = document.createElement('li');
+            areaItem.textContent = area.name;
+
+            const checklist = document.createElement('ul');
+            area.checklist.forEach((item) => {
+                const checklistItem = document.createElement('li');
+                const checkbox = document.createElement('input');
+                checkbox.type = 'checkbox';
+                checkbox.checked = item.completed;
+                checklistItem.appendChild(checkbox);
+                checklistItem.appendChild(document.createTextNode(item.text));
+                checklist.appendChild(checklistItem);
+            });
+
+            areaItem.appendChild(checklist);
+            areasList.appendChild(areaItem);
+        });
+
+        actItem.appendChild(areasList);
+        actsContainer.appendChild(actItem);
+    });
+}
+
+// Event listener for marking items as completed
+actsContainer.addEventListener('change', (event) => {
+    if (event.target.type === 'checkbox') {
+        // Find the corresponding checklist item and update its completion status
+        const text = event.target.nextElementSibling.textContent;
+        checklistData.acts.forEach((act) => {
+            act.areas.forEach((area) => {
+                area.checklist.forEach((item) => {
+                    if (item.text === text) {
+                        item.completed = event.target.checked;
+                        // Save the data to localStorage or your backend here
+                    }
+                });
+            });
+        });
+    }
+});
+
+// Initial rendering
+renderChecklist();
