255ed22075bec7875938e863b35f97a41cb0b88b
alepocalypse <48845086+alepocalypse@users.noreply.github.com>
2023-08-31 19:32:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,70 +1,47 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-import checklistData from './ChecklistData.js';
-
-function saveChecklist() {
-    // Save checklist items to local storage
-    const items = Array.from(checklist.children).map((item) => ({
-        text: item.querySelector('span').textContent,
-        completed: item.querySelector('input').checked,
-    }));
-
-    localStorage.setItem('checklistItems', JSON.stringify(items));
-}
-
-const actsContainer = document.getElementById('acts');
-
-// Function to render the checklist
+// Function to fetch JSON data and render the checklist
 function renderChecklist() {
-    actsContainer.innerHTML = '';
+    const checklistContainer = document.getElementById('checklist');
 
-    checklistData.acts.forEach((act) => {
-        const actItem = document.createElement('li');
-        actItem.textContent = act.name;
+    // Fetch the JSON data
+    fetch('checklistData.json')
+        .then(response => response.json())
+        .then(data => {
+            // Use the fetched data to render the checklist
+            data.acts.forEach(act => {
+                const actItem = document.createElement('li');
+                actItem.textContent = act.name;
 
-        const areasList = document.createElement('ul');
-        act.areas.forEach((area) => {
-            const areaItem = document.createElement('li');
-            areaItem.textContent = area.name;
+                const areasList = document.createElement('ul');
+                act.areas.forEach(area => {
+                    const areaItem = document.createElement('li');
+                    areaItem.textContent = area.name;
 
-            const checklist = document.createElement('ul');
-            area.checklist.forEach((item) => {
-                const checklistItem = document.createElement('li');
-                const checkbox = document.createElement('input');
-                checkbox.type = 'checkbox';
-                checkbox.checked = item.completed;
-                checklistItem.appendChild(checkbox);
-                checklistItem.appendChild(document.createTextNode(item.text));
-                checklist.appendChild(checklistItem);
+                    const checklist = document.createElement('ul');
+                    area.checklist.forEach(item => {
+                        const checklistItem = document.createElement('li');
+                        const checkbox = document.createElement('input');
+                        checkbox.type = 'checkbox';
+                        checkbox.checked = item.completed;
+                        checklistItem.appendChild(checkbox);
+                        checklistItem.appendChild(document.createTextNode(item.text));
+                        checklist.appendChild(checklistItem);
+                    });
+
+                    areaItem.appendChild(checklist);
+                    areasList.appendChild(areaItem);
+                });
+
+                actItem.appendChild(areasList);
+                checklistContainer.appendChild(actItem);
             });
-
-            areaItem.appendChild(checklist);
-            areasList.appendChild(areaItem);
+        })
+        .catch(error => {
+            console.error('Error loading checklist data:', error);
         });
-
-        actItem.appendChild(areasList);
-        actsContainer.appendChild(actItem);
-    });
 }
 
-// Event listener for marking items as completed
-actsContainer.addEventListener('change', (event) => {
-    if (event.target.type === 'checkbox') {
-        // Find the corresponding checklist item and update its completion status
-        const text = event.target.nextElementSibling.textContent;
-        checklistData.acts.forEach((act) => {
-            act.areas.forEach((area) => {
-                area.checklist.forEach((item) => {
-                    if (item.text === text) {
-                        item.completed = event.target.checked;
-                        saveChecklist();
-                    }
-                });
-            });
-        });
-    }
-});
-
-// Initial rendering
+// Call the renderChecklist function to load and render the data
 renderChecklist();
-});
+    });
